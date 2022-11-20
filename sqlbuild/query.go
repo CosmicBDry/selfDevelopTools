b611004dd248c1ac153ff3c8228c3d5518be267d
CosmicBDry <48923904+CosmicBDry@users.noreply.github.com>
2022-11-20 23:55:53 +0800

    Update query.go

--- sqlbuild/query.go
+++ sqlbuild/query.go
@@ -8,7 +8,7 @@ import (
 //合成sql查询语句
 func (b *Builder) BuildQuery() string {
 	if b.base == "" {
-		fmt.Println("no sql intput")
+		fmt.Println("no sqlTemplate input")
 		return ""
 	}
 	return strings.TrimSpace(b.base + " " + strings.Join(b.whereStmt, " ") +
